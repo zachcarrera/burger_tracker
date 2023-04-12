1b1fc0e9180fb369d7292dc519194b16fcbacda4
Zach Carrera <zachcarrera@gmail.com>
2023-04-12 11:07:58 -0700

    update BurgerRepository with the repository annotation

--- src/main/java/com/zachcarrera/burgertracker/repositories/BurgerRepository.java
+++ src/main/java/com/zachcarrera/burgertracker/repositories/BurgerRepository.java
@@ -3,9 +3,11 @@ package com.zachcarrera.burgertracker.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.zachcarrera.burgertracker.models.Burger;
 
+@Repository
 public interface BurgerRepository extends CrudRepository<Burger, Long>{
 	
 	List<Burger> findAll();
