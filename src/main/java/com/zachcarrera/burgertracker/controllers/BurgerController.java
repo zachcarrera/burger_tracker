bee8ac97167d8a02b8a902f2e228ccb421cf5de2
Zach Carrera <zachcarrera@gmail.com>
2023-04-13 17:10:26 -0700

    add routes to render and process the burger edit form in BurgerController

--- src/main/java/com/zachcarrera/burgertracker/controllers/BurgerController.java
+++ src/main/java/com/zachcarrera/burgertracker/controllers/BurgerController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.zachcarrera.burgertracker.models.Burger;
 import com.zachcarrera.burgertracker.services.BurgerService;
@@ -17,7 +19,7 @@ import jakarta.validation.Valid;
 
 @Controller
 public class BurgerController {
-	
+
 	@Autowired
 	private BurgerService burgerService;
 
@@ -27,7 +29,7 @@ public class BurgerController {
 		model.addAttribute("burgers", burgers);
 		return "index.jsp";
 	}
-	
+
 	@PostMapping("/burgers/new")
 	public String create(Model model, @Valid @ModelAttribute("newBurger") Burger newBurger, BindingResult result) {
 		if (result.hasErrors()) {
@@ -38,4 +40,30 @@ public class BurgerController {
 		burgerService.createBurger(newBurger);
 		return "redirect:/";
 	}
+
+	@GetMapping("/burgers/edit/{id}")
+	public String renderEdit(@PathVariable("id") Long id, Model model) {
+		
+		Burger oneBurger = burgerService.findBurger(id);
+		model.addAttribute("oneBurger", oneBurger);
+	
+		return "edit.jsp";
+		
+	}
+
+	@PutMapping("/burgers/edit/{id}")
+	public String processEdit(
+			@PathVariable("id") Long id,
+			@Valid @ModelAttribute("oneBurger") Burger oneBurger,
+			BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "edit.jsp";
+		}
+		
+		burgerService.updateBurger(oneBurger);
+
+		return "redirect:/";
+	}
+	
 }
